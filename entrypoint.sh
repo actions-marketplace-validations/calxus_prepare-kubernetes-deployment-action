eca21bbff9e7c63d186997753c01ad22910bfb3c
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-16 20:33:25 +0000

    Adding the inputs of environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pip3 install -r /requirements.txt
-python3 /prepare.py --service-name $1 --service-replica-count $2 --image-name $3 --image-namespace $4 --image-version $5 --service-envvars
+python3 /prepare.py --service-name $1 --service-replica-count $2 --image-name $3 --image-namespace $4 --image-version $5 --service-envvars $6
 cp -rp /service service
