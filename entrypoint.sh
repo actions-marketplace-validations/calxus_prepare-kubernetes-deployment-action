76ec277a8986d73470cc001f70be97c627530f21
Gordon Adam-Santiago <gtadam@protonmail.ch>
2020-12-16 21:22:27 +0000

    Adding debug capabilities

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 pip3 install -r /requirements.txt
-python3 /prepare.py --service-name $1 --service-replica-count $2 --image-name $3 --image-namespace $4 --image-version $5 --service-envvars $6
+python3 /prepare.py --service-name $2 --service-replica-count $3 --image-name $4 --image-namespace $5 --image-version $6 --service-envvars $7
+if [ "$1" = true ] ; then
+    cat /service/values.yaml
+fi
 cp -rp /service service
